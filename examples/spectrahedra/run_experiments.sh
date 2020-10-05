c4582b4d87c72dca400d913dd892c01053937be8
Tolis <tolis.chal@gmail.com>
2020-10-05 17:39:06 +0300

    improve eigenvalue computation

---
+++ examples/spectrahedra/run_experiments.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+./test_sdp -sdp ../../../data/sdp_prob_20_20.txt -opt_val -2.91067 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_40_40.txt -opt_val -1.484304 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_60_60.txt -opt_val -1.417085 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_80_80.txt -opt_val -1.371746 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_100_100.txt -opt_val -1.165252 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_120_120.txt -opt_val -1.050110 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_140_140.txt -opt_val -0.923046 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_160_160.txt -opt_val -0.942309 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_180_180.txt -opt_val -0.940935 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_200_200.txt -opt_val -1.01898 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_220_220.txt -opt_val -0.831538 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_240_240.txt -opt_val -0.85435 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_260_260.txt -opt_val -0.915454 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_280_280.txt -opt_val -0.84546 -error 0.01 -k 0.25
+./test_sdp -sdp ../../../data/sdp_prob_300_300.txt -opt_val -0.80723 -error 0.01 -k 0.25
--- include/matrix_operations/DenseProductMatrix.h
+++ include/matrix_operations/DenseProductMatrix.h
@@ -11,6 +11,7 @@
 #define VOLESTI_DENSEPRODUCTMATRIX_H
 
 #define PARTIAL_LU_DECOMPOSITION
+#define CHOLESKY__NO_PIVOTING_DECOMPOSITION
 
 /// A wrapper class for dense Eigen matrices in Spectra and ARPACK++
 /// This class will be the wrapper to use the Spectra nonsymemmetric standard eigenvalue Cx = lx solver to
@@ -38,27 +39,39 @@ public:
 
     VT v;
 
+    int m;
+
     /// The decomposition we will use
     /// If PARTIAL_LU_DECOMPOSITION is defined, use the Eigen partial LU decomposition,
     /// otherwise full. The partial is faster but assumes that the matrix has full rank.
 #if defined(PARTIAL_LU_DECOMPOSITION)
-    typedef Eigen::PartialPivLU<MT> Decomposition;
+    typedef Eigen::PartialPivLU<MT> lu_decomposition;    
 #else
-    typedef Eigen::FullPivLU<MT> Decomposition;
+    typedef Eigen::FullPivLU<MT> lu_decomposition;
+#endif
+
+#if defined(CHOLESKY__NO_PIVOTING_DECOMPOSITION)
+    //typedef Eigen::PartialPivLU<MT> cholesky_decomposition;
+    typedef Eigen::LDLT<MT, Eigen::Upper> cholesky_decomposition;
+#else
+    typedef Eigen::LDLT<MT> cholesky_decomposition;
 #endif
 
     /// The LU decomposition of B
-    Decomposition Blu;
-    //LLT<MT> llt;
+    //lu_decomposition Blu;
+    cholesky_decomposition llt;
 
     /// Constructs an object of this class and computes the LU decomposition of B.
     ///
     /// \param[in] A The matrix A
     /// \param[in] B The matrix B
     DenseProductMatrix(MT const *A, MT const *B) : A(A), B(B) {
-        Blu.compute(*B);
+        //Blu.compute(*B);
+        
         _rows = A->rows();
         _cols = B->cols();
+        m = _cols / 2;
+        llt.compute((*B).block(0, m, m, m));
 
         v.setZero(_rows);
     }
@@ -90,9 +103,14 @@ public:
         int r = _rows / 2;
         v.block(r, 0, r, 1).noalias() = (*A).block(r, r, r, r).template selfadjointView< Eigen::Upper >() * x.block(r, 0, r, 1);
         v.block(0, 0, r, 1).noalias() = (*A).block(0, 0, r, r).template selfadjointView< Eigen::Upper >() * x.block(0, 0, r, 1);
+        v = -v;
 
         Eigen::Map<VT> y(y_out, _rows);
-        y.noalias() = Blu.solve(-v);
+        //y.noalias() = Blu.solve(-v);
+
+        y.block(0, 0, r, 1).noalias() = llt.solve(v.block(r, 0, r, 1));
+        v.block(0, 0, r, 1).noalias() -= (*B).block(0, 0, r, r) * y.block(0, 0, r, 1);
+        y.block(r, 0, r, 1).noalias() = llt.solve(v.block(0, 0, r, 1));
     }
 
     /// Required by arpack.
@@ -111,9 +129,24 @@ public:
     
         v.block(r, 0, r, 1).noalias() = (*A).block(r, r, r, r).template selfadjointView< Eigen::Upper >() * x.block(r, 0, r, 1);
         v.block(0, 0, r, 1).noalias() = (*A).block(0, 0, r, r).template selfadjointView< Eigen::Upper >() * x.block(0, 0, r, 1);
+        v = -v;
 
         Eigen::Map<VT> y(y_out, _rows);
-        y.noalias() = Blu.solve(-v);
+        //y.noalias() = Blu.solve(v);
+
+        //VT yy(_rows);
+        //std::cout<<"\n B = "<<(*B)<<"\n"<<std::endl;
+        //std::cout<<"B.block(0, m, m, m) = "<<(*B).block(0, r, r, r)<<"\n"<<std::endl;
+        //std::cout<<"v = "<<v.transpose()<<std::endl;
+        y.block(0, 0, r, 1).noalias() = llt.solve(v.block(r, 0, r, 1));
+        v.block(0, 0, r, 1).noalias() -= (*B).block(0, 0, r, r) * y.block(0, 0, r, 1);
+        y.block(r, 0, r, 1).noalias() = llt.solve(v.block(0, 0, r, 1));
+        
+
+        //std::cout<<"y = "<<y.transpose()<<std::endl;
+        //std::cout<<"yy = "<<yy.transpose()<<"\n"<<std::endl;
+
+        //y=yy;
     }
 };
 #endif //VOLESTI_DENSEPRODUCTMATRIX_H
