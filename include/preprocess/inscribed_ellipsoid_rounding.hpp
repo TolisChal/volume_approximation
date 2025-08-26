3e84e8af4e1020340f0ac92db4dfdf7753e7b0da
Apostolos Chalkis <tolis.chal@gmail.com>
2025-08-27 01:45:29 +0300

    add log barrier as rounding method

--- include/preprocess/inscribed_ellipsoid_rounding.hpp
+++ include/preprocess/inscribed_ellipsoid_rounding.hpp
@@ -1,7 +1,7 @@
 // VolEsti (volume computation and sampling library)
 
-// Copyright (c) 2012-2020 Vissarion Fisikopoulos
-// Copyright (c) 2018-2020 Apostolos Chalkis
+// Copyright (c) 2012-2024 Vissarion Fisikopoulos
+// Copyright (c) 2018-2024 Apostolos Chalkis
 
 //Contributed and/or modified by Alexandros Manochis, as part of Google Summer of Code 2020 program.
 
@@ -11,7 +11,34 @@
 #ifndef INSCRIBED_ELLIPSOID_ROUNDING_HPP
 #define INSCRIBED_ELLIPSOID_ROUNDING_HPP
 
-#include "max_inscribed_ellipsoid.hpp"
+#include "preprocess/max_inscribed_ellipsoid.hpp"
+#include "preprocess/analytic_center_linear_ineq.h"
+#include "preprocess/feasible_point.hpp"
+
+enum EllipsoidType
+{
+  MAX_ELLIPSOID = 1,
+  LOG_BARRIER = 2
+};
+
+template<typename MT, int ellipsoid_type, typename Custom_MT, typename VT, typename NT>
+inline static std::tuple<MT, VT, NT>
+compute_inscribed_ellipsoid(Custom_MT A, VT b, VT const& x0,
+                            unsigned int const& maxiter,
+                            NT const& tol, NT const& reg)
+{
+    if constexpr (ellipsoid_type == EllipsoidType::MAX_ELLIPSOID)
+    {
+        return max_inscribed_ellipsoid<MT>(A, b, x0, maxiter, tol, reg);
+    } else if constexpr (ellipsoid_type == EllipsoidType::LOG_BARRIER)
+    {
+        return analytic_center_linear_ineq<MT, Custom_MT, VT, NT>(A, b, x0);
+    } else
+    {
+        std::runtime_error("Unknown rounding method.");
+    }
+    return {};
+}
 
 template 
 <
@@ -19,37 +46,50 @@ template
     typename VT,
     typename NT,
     typename Polytope,
-    typename Point   
+    int ellipsoid_type = EllipsoidType::MAX_ELLIPSOID
 >
 std::tuple<MT, VT, NT> inscribed_ellipsoid_rounding(Polytope &P, 
-                                                        Point const& InnerPoint,
-                                                        unsigned int const max_iterations = 5,
-                                                        NT const max_eig_ratio = NT(6))
+                                                    unsigned int const max_iterations = 5,
+                                                    NT const max_eig_ratio = NT(6))
 {
-    std::pair<std::pair<MT, VT>, bool> iter_res;
-    iter_res.second = false;
+    typedef typename Polytope::PointType Point;
+    VT x = compute_feasible_point(P.get_mat(), P.get_vec());
+    return inscribed_ellipsoid_rounding<MT, VT, NT>(P, Point(x), max_iterations, max_eig_ratio);
+}
 
-    VT x0 = InnerPoint.getCoefficients();
-    MT E, L;
+template 
+<
+    typename MT,
+    typename VT,
+    typename NT,
+    typename Polytope,
+    typename Point,
+    int ellipsoid_type = EllipsoidType::MAX_ELLIPSOID
+>
+std::tuple<MT, VT, NT> inscribed_ellipsoid_rounding(Polytope &P, 
+                                                    Point const& InnerPoint,
+                                                    unsigned int const max_iterations = 5,
+                                                    NT const max_eig_ratio = NT(6))
+{
     unsigned int maxiter = 500, iter = 1, d = P.dimension();
-
+    VT x0 = InnerPoint.getCoefficients(), center, shift = VT::Zero(d);
+    MT E, L, T = MT::Identity(d, d);
+    bool converged;
     NT R = 100.0, r = 1.0, tol = std::pow(10, -6.0), reg = std::pow(10, -4.0), round_val = 1.0;
-
-    MT T = MT::Identity(d, d);
-    VT shift = VT::Zero(d);
 
     while (true)
     {
-        // compute the largest inscribed ellipsoid in P centered at x0
-        iter_res = max_inscribed_ellipsoid<MT>(P.get_mat(), P.get_vec(), x0, maxiter, tol, reg);
-        E = iter_res.first.first;
+        // Compute the desired inscribed ellipsoid in P
+        std::tie(E, center, converged) = 
+            compute_inscribed_ellipsoid<MT, ellipsoid_type>(P.get_mat(), P.get_vec(), x0, maxiter, tol, reg);
+        
         E = (E + E.transpose()) / 2.0;
-        E = E + MT::Identity(d, d)*std::pow(10, -8.0); //normalize E
+        E += MT::Identity(d, d)*std::pow(10, -8.0); //normalize E
 
         Eigen::LLT<MT> lltOfA(E.llt().solve(MT::Identity(E.cols(), E.cols()))); // compute the Cholesky decomposition of E^{-1}
         L = lltOfA.matrixL();
 
-        // computing eigenvalues of E
+        // Computing eigenvalues of E
         Spectra::DenseSymMatProd<NT> op(E);
         // The value of ncv is chosen empirically
         Spectra::SymEigsSolver<NT, Spectra::SELECT_EIGENVALUE::BOTH_ENDS, 
@@ -59,7 +99,7 @@ std::tuple<MT, VT, NT> inscribed_ellipsoid_rounding(Polytope &P,
         if (eigs.info() == Spectra::COMPUTATION_INFO::SUCCESSFUL) {
             R = 1.0 / eigs.eigenvalues().coeff(1);
             r = 1.0 / eigs.eigenvalues().coeff(0);
-        } else  {
+        } else {
             Eigen::SelfAdjointEigenSolver<MT> eigensolver(E);
             if (eigensolver.info() == Eigen::ComputationInfo::Success) {
                 R = 1.0 / eigensolver.eigenvalues().coeff(0);
@@ -68,10 +108,10 @@ std::tuple<MT, VT, NT> inscribed_ellipsoid_rounding(Polytope &P,
                 std::runtime_error("Computations failed.");
             }
         }
-        // shift polytope and apply the linear transformation on P
-        P.shift(iter_res.first.second);
-        shift += T * iter_res.first.second;
-        T = T * L;
+        // Shift polytope and apply the linear transformation on P
+        P.shift(center);
+        shift.noalias() += T * center;
+        T.applyOnTheRight(L); // T = T * L;
         round_val *= L.transpose().determinant();
         P.linear_transformIt(L);
 
@@ -79,8 +119,8 @@ std::tuple<MT, VT, NT> inscribed_ellipsoid_rounding(Polytope &P,
         P.normalize();
         x0 = VT::Zero(d);
 
-        // check the roundness of the polytope
-        if(((std::abs(R / r) <= max_eig_ratio && iter_res.second) || iter >= max_iterations)){
+        // Check the roundness of the polytope
+        if(((std::abs(R / r) <= max_eig_ratio && converged) || iter >= max_iterations)) {
             break;
         }
 
